71df9ed0c12c4f044bb08e25f0bec57b8451b8ac
Ayman-Malkawi <aymanmalkawi592@gmail.com>
2025-01-16 17:23:07 +0300

    any cooment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,69 @@ namespace ConsoleApp1
         static void Main(string[] args)
     
         {
-            Console.WriteLine("ayman");
-            Console.ReadLine();
+            Console.WriteLine("inter your value");
+            string value=  Console.ReadLine();
+            Console.WriteLine("your input :  "+value);
+
+
+            //////////////////////////////////////
+            ///
+
+            double d = 5.5;
+            Console.WriteLine("double is : " + d);
+            string s = "ayman";
+            Console.WriteLine("string is : " + s);
+            char c = 'a';
+            Console.WriteLine("char is : " + c);
+            bool b = true;
+            Console.WriteLine("bool is : " + b);
+            int i = 0;
+            Console.WriteLine("int is : " + i);
+            const int j = 1;
+            Console.WriteLine("const is : " + j);
+
+            ////////////////////////////////////////
+            ///
+
+            string[] car = { "bmw", "merceds", "porcha" };
+            for (int x = 0; x < car.Length; x++)
+            {
+                Console.WriteLine(car[x]);
+            }
+
+            /////////////////////////////////
+            ///
+            Console.WriteLine("inter Fname");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("inter Lname");
+
+            string lastname = Console.ReadLine();
+            Console.WriteLine("inter birth");
+
+            string dateofbirth = Console.ReadLine();
+            Console.WriteLine(firstname+" "+lastname+" "+dateofbirth);
+
+            ///////////
+            ///
+            string[]first=new string[10];
+            for (int x = 0; x < first.Length; x++)
+            { 
+                Console.WriteLine("put in array");
+                string aaaa = Console.ReadLine();
+                first[x] = aaaa;
+
+
+            }foreach (string q in first)
+            {
+
+                Console.WriteLine(q);
+            }
+
+
+
         }
+        
     }
 }
+    
