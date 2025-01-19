5704842b8727052dc182fac985b0c54731bfe718
Ayman-Malkawi <aymanmalkawi592@gmail.com>
2025-01-19 21:59:51 +0300

    task2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,56 +19,56 @@ namespace ConsoleApp1
             //////////////////////////////////////
             ///
 
-            double d = 5.5;
-            Console.WriteLine("double is : " + d);
-            string s = "ayman";
-            Console.WriteLine("string is : " + s);
-            char c = 'a';
-            Console.WriteLine("char is : " + c);
-            bool b = true;
-            Console.WriteLine("bool is : " + b);
-            int i = 0;
-            Console.WriteLine("int is : " + i);
-            const int j = 1;
-            Console.WriteLine("const is : " + j);
+            //double d = 5.5;
+            //Console.WriteLine("double is : " + d);
+            //string s = "ayman";
+            //Console.WriteLine("string is : " + s);
+            //char c = 'a';
+            //Console.WriteLine("char is : " + c);
+            //bool b = true;
+            //Console.WriteLine("bool is : " + b);
+            //int i = 0;
+            //Console.WriteLine("int is : " + i);
+            //const int j = 1;
+            //Console.WriteLine("const is : " + j);
 
             ////////////////////////////////////////
             ///
 
-            string[] car = { "bmw", "merceds", "porcha" };
-            for (int x = 0; x < car.Length; x++)
-            {
-                Console.WriteLine(car[x]);
-            }
+            //string[] car = { "bmw", "merceds", "porcha" };
+            //for (int x = 0; x <= car.Length; x++)
+            //{
+            //    Console.WriteLine(car[x]);
+            //}
 
             /////////////////////////////////
             ///
-            Console.WriteLine("inter Fname");
-            string firstname = Console.ReadLine();
+            //Console.WriteLine("inter Fname");
+            //string firstname = Console.ReadLine();
 
-            Console.WriteLine("inter Lname");
+            //Console.WriteLine("inter Lname");
 
-            string lastname = Console.ReadLine();
-            Console.WriteLine("inter birth");
+            //string lastname = Console.ReadLine();
+            //Console.WriteLine("inter birth");
 
-            string dateofbirth = Console.ReadLine();
-            Console.WriteLine(firstname+" "+lastname+" "+dateofbirth);
+            //string dateofbirth = Console.ReadLine();
+            //Console.WriteLine(firstname+" "+lastname+" "+dateofbirth);
 
             ///////////
             ///
-            string[]first=new string[10];
-            for (int x = 0; x < first.Length; x++)
-            { 
-                Console.WriteLine("put in array");
-                string aaaa = Console.ReadLine();
-                first[x] = aaaa;
+            //string[]first=new string[10];
+            //for (int x = 0; x <= first.Length; x++)
+            //{ 
+            //    Console.WriteLine("put in array");
+            //    string aaaa = Console.ReadLine();
+            //    first[x] = aaaa;
 
 
-            }foreach (string q in first)
-            {
+            //}foreach (string q in first)
+            //{
 
-                Console.WriteLine(q);
-            }
+            //    Console.WriteLine(q);
+            //}
 
 
 
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,16 +1,161 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System;
+using System.Runtime.Remoting.Channels;
+using System.ComponentModel.Design.Serialization;
 namespace ConsoleApp2
 {
     internal class Program
     {
+     
+
+        //static int num5(int min, int h)
+        //{
+        //    return (h * 60) + min;
+        //}
+
+
+
+        //static double number5(double x)
+        //{
+        //    return x * 0.621371;
+        //}
         static void Main(string[] args)
         {
-            
+
+            //task1
+            //double myDouble;
+            //myDouble = Convert.ToDouble(Console.ReadLine());
+            //int myInt = (int)myDouble;
+            //Console.WriteLine(myInt);
+
+            //task2
+            //int hola = 25;
+            //Console.WriteLine("your number is : " +Convert.ToString(hola));
+
+            //task3
+            //string upper = "i love c#";
+            //string uppercase= upper.ToUpper();
+            //string lower =upper.ToLower();
+            //Console.WriteLine("orginal string"+ upper);
+            //Console.WriteLine("upper " + uppercase);
+            //Console.WriteLine("lower " + lower);
+
+            //task4
+            //string fullname = "ayman malkawi";
+            //string Fname = fullname.Substring(0,5);
+            //string Lname = fullname.Substring(5);
+            //Console.WriteLine("First Name: "+Fname);
+            //Console.WriteLine("Last Name: "+ Lname);
+            //Console.WriteLine("Total Characters: "+ fullname.Length);
+
+            //task5
+            //int num3 = 3;
+            //int num4 = 9;
+            //if (num3 < num4);
+            //{
+            //    Console.WriteLine("smaler number is : " + num3);
+            //}
+
+
+            //task6
+            //Console.WriteLine(number5(15));
+
+
+            //task7
+            //Console.WriteLine(num5(5, 6));
+
+            //task8
+
+            //Console.WriteLine("Enter minutes : ");
+            //int m = Convert.ToInt32(Console.ReadLine());//546
+            //int h = m / 60;//9
+            //int s = m % 60;//6
+            //Console.WriteLine(h + " Hours, " + s + " Minutes");
+
+            //task9
+
+            // Console.WriteLine("num1 : ");
+            // int m = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("num2 : ");
+            // int s = Convert.ToInt32(Console.ReadLine());
+            // if (m == s)
+            // {
+            //     Console.WriteLine("equals");
+            // }
+            //else if (m > s)
+            // {
+            //     Console.WriteLine("Greater");
+            // }
+            // else if (m < s)
+            // {
+            //     Console.WriteLine("Smaller");
+            // }
+
+
+            //Console.WriteLine("the last number is :");
+            //string number = Console.ReadLine();//123
+            //int x = 0;
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    x=x+ Convert.ToInt32 (number[i].ToString());
+            //}
+            //Console.WriteLine(x);
+
+
+
+            //task10
+
+            //Console.WriteLine("insert numb :");
+            //string noom = Console.ReadLine();
+            //string s = "";
+            //for (int i = noom.Length - 1; i >= 0; i--) {
+
+            //    s = s + noom[i].ToString();
+
+            //}
+            //Console.WriteLine(s);
+
+
+            //task11
+            //Console.WriteLine("insert x :");
+            //int x = Convert.ToInt32 (Console.ReadLine());
+            //Console.WriteLine("insert y:");
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //if (x % y == 0)
+            //{
+            //    Console.WriteLine("Divisible");
+            //}
+            //else { Console.WriteLine("Not Divisible"); }
+
+            //task12
+            //Console.WriteLine("firt number :");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("second number :");
+            //int num2= Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("third :");
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+
+            //int min = Math.Min(num1,Math.Min(num2,num3));
+
+            //int max = Math.Max(num1, Math.Max(num2, num3));
+
+            //Console.WriteLine("the middle num is : " + (num1+num2+num3-min-max));
+
+
         }
+
+
     }
-}
+
+
+
+    }
+
+
+       
+       
+       
+
