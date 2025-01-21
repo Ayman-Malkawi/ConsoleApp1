2d0ba0ae6023d6e0667e752c29c3102fcdb74d2b
Ayman-Malkawi <aymanmalkawi592@gmail.com>
2025-01-21 21:55:06 +0300

    task4

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -3,93 +3,155 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApp5;
 
 namespace ConsoleApp5
 {
+
+
+    public class hotel
+    {
+        public int roomNumber;//field
+        public string roomType;//field
+        public double price;//field
+        public bool isBooked;//field
+        public const string hotelName = "Hotel";//Constant
+};
+  
+      
+    }
     internal class Program
     {
+        //public static void jana()
+        //{
+        //    int x = 27;
+        //    for (int i = 2; i < x; i++) 
+        //    {
+        //        if (x % i == 0)
+        //        {
+        //            Console.WriteLine("Not Prime");
+        //            break;
+        //        }
+        //        else
+        //        {
+
+        //            Console.WriteLine("prime");
+        //            break;
+
+        //        }
+        //    }
+        //}
 
 
-        static void Main(string[] args)
-        {
-
-            //task1
-            //Console.WriteLine("inter the number");
-            //int num = int.Parse(Console.ReadLine());
-            //if (num % 2 == 0)
-            //{
-            //    Console.WriteLine("even");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("odd");
-
-            //}  
-
-            //task2
-            //Console.WriteLine("Enter 5 numbers!");
-            //int[] numbr = new int[5]; 
-
-            //for (int i = 0; i < numbr.Length; i++)
-            //{
-            //    Console.Write($"Enter number {i}: ");
-            //    numbr[i] = int.Parse(Console.ReadLine());
-
-            //}
-
-            //Array.Sort(numbr);
-
-            //Console.WriteLine($"The second smallest number is: {numbr[1]}");
-
-            //task3
-            //Console.Write("Enter a number: ");
-            //int number = int.Parse(Console.ReadLine());
-
-            //int result = 1;
-
-            //for (int i = 1; i <= number; i++)
-            //{
-            //    result = result * i;
-            //}
-
-            //Console.WriteLine($"The factorial of {number} is: {result}");
-
-
-            //task4 not completed
-            //Console.WriteLine("insert number");
-            //int numb = int.Parse(Console.ReadLine());
+    static void Main(string[] args)
+    {
+        //jana();
 
 
 
 
-            //task5
-            //Console.WriteLine("number is");
-            //int[] numbers = { 3, 1, 4, 1, 5, 9 };
-            //int larg = numbers[0];
-            //foreach (int i in numbers) {
-            //    if (i > larg)
-            //    {
-            //        larg= i;
-            //    }
 
-            //    Console.WriteLine(i);
-            //}
 
-            //task6
-            //int n = 5; 
-            //int number = 1;
 
-            //for (int i = 1; i <= n; i++)
-            //{
-            //    for (int j = 1; j <= i; j++)
-            //    {
-            //        Console.Write(number + " ");
-            //        number++;
-            //    }
-            //    Console.WriteLine();
 
-            //}
+        //task1
+        //Console.WriteLine("inter the number");
+        //int num = int.Parse(Console.ReadLine());
+        //if (num % 2 == 0)
+        //{
+        //    Console.WriteLine("even");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("odd");
 
-            }
+        //}  
+
+        //task2
+        //Console.WriteLine("Enter 5 numbers!");
+        //int[] numbr = new int[5]; 
+
+        //for (int i = 0; i < numbr.Length; i++)
+        //{
+        //    Console.Write($"Enter number {i}: ");
+        //    numbr[i] = int.Parse(Console.ReadLine());
+
+        //}
+
+        //Array.Sort(numbr);
+
+        //Console.WriteLine($"The second smallest number is: {numbr[1]}");
+
+        //task3
+        //Console.Write("Enter a number: ");
+        //int number = int.Parse(Console.ReadLine());
+
+        //int result = 1;
+
+        //for (int i = 1; i <= number; i++)
+        //{
+        //    result = result * i;
+        //}
+
+        //Console.WriteLine($"The factorial of {number} is: {result}");
+
+
+        //task4 not completed
+        //Console.WriteLine("insert number");
+        //int numb = int.Parse(Console.ReadLine());
+
+
+
+
+        //task5
+        //Console.WriteLine("number is");
+        //int[] numbers = { 3, 1, 4, 1, 5, 9 };
+        //int larg = numbers[0];
+        //foreach (int i in numbers) {
+        //    if (i > larg)
+        //    {
+        //        larg= i;
+        //    }
+
+        //    Console.WriteLine(i);
+        //}
+
+        //task6
+        //int n = 5; 
+        //int number = 1;
+
+        //for (int i = 1; i <= n; i++)
+        //{
+        //    for (int j = 1; j <= i; j++)
+        //    {
+        //        Console.Write(number + " ");
+        //        number++;
+        //    }
+        //    Console.WriteLine();
+
+        //}
+        //task7
+        hotel Room1 = new hotel();
+        Room1.roomNumber = Convert.ToInt32(Console.ReadLine());
+        Room1.roomType = Console.ReadLine();
+        Room1.price = Convert.ToDouble(Console.ReadLine());
+        Room1.isBooked = Convert.ToBoolean(Console.ReadLine());
+        //hotel.hotelName = Console.ReadLine();
+        Console.WriteLine($"{Room1.roomNumber} {Room1.roomType} {Room1.price} {Room1.isBooked} {hotel.hotelName}");
+        //---------------------------------------------
+        Console.WriteLine(hotel.hotelName);
+        hotel Room2 = new hotel();
+        Room2.roomNumber = Convert.ToInt32(Console.ReadLine());
+        Room2.roomType = Console.ReadLine();
+        Room2.price = Convert.ToDouble(Console.ReadLine());
+        Room2.isBooked = Convert.ToBoolean(Console.ReadLine());
+        //hotel.hotelName = Console.ReadLine();
+        Console.WriteLine(hotel.hotelName);
+        Console.WriteLine($"{Room2.roomNumber} {Room2.roomType} {Room2.price} {Room2.isBooked} {hotel.hotelName}");
+
+
+
+    
+
     }
-}
+    }
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Collections.Specialized.BitVector32;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        public class student
+        {
+
+            ~student() { Console.WriteLine("the program end"); }
+
+
+            public student()
+            {
+                name = "";
+                age = 1;
+                id = 1;
+
+
+            }
+            public student(string name, int age , int id)
+            {
+                this.name = name;
+                Age = age;
+                this.id = id;
+
+
+            }
+
+            public void GetDetails()
+            {
+
+                Console.WriteLine($"name : {name}\nage : {age}\nid :{id}");
+            }
+
+
+
+            public string name {  get; set; }
+
+            private int age;
+            public int Age {  
+                get { return age; }
+                set
+                {
+                    if (value >= MinAge && value <= MaxAge)
+                    {
+                        age = value;
+                    }
+                    else
+                    {
+                        age = 0;
+                    }
+                }
+                }
+            public int id {  get; set; }
+            public string email { get; set; }
+            public const int MinAge = 18;
+            public const int MaxAge = 40;
+         
+        }
+        static void Main(string[] args)
+        {
+            student number1 = new student();
+            number1.name = "ahmed";
+            number1.Age = 22;
+            number1.id = 1001;
+
+            number1.GetDetails();
+
+
+            student number2 = new student("ayman" , 17 , 1002);
+            number2.GetDetails();
+
+
+
+        }
+    }
+}
+
+
+//1- A class is a template or blueprint we use it to make the code more organized
+//2- An object is an instance of the class to spacify values to the class properties
+//3- An object is an instance of the class (Part Of It)
+//4- Encapsulation, Abstract, polymorphism and inheritance
+//5- A property is a way to encabsulate the class feild so we can print or modify the feilds without a direct access to it
+//6- A feild is a normal variable declared inside a class.
+//7- A Constructor is a special method to initialize the object feilds and it will be executed automatically when we create an object
+//8- Encapsulation is one of the OOP principles and it means to hide what is supposed to be hidden and only show what is neces
+
